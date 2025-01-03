2daa338d66ac73fc70470c49ae8cd168572030c9
VeronaDanzka <davide.denotarpietro@gmail.com>
2025-01-03 04:10:24 +0100

    first commit

---
+++ kasa-app/src/pages/404.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return(
+        <div> 
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a>Retourner sur la page d'accueil</a>
+        </div>
+    )
+}
+
+export default NotFound
