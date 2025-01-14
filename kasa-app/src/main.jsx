9e4c8436acaa9ac1a0d479c38b6758aa22b2fbec
VeronaDanzka <davide.denotarpietro@gmail.com>
2025-01-14 14:57:59 +0100

    retouche finale

--- kasa-app/src/main.jsx
+++ kasa-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import React Router
+import { BrowserRouter } from 'react-router-dom'; // Import React Router
 import './assets/style.css'
 import App from './App.jsx'
 
